38a43d9596642d85389138bb026b598c2ea1d6f2
AmadorZcv <lucasamadorzcv@gmail.com>
2019-10-05 17:13:09 -0300

    Função de load token

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import store from './app/redux/store';
-import {ThemeProvider} from 'react-native-elements';
+import {ThemeProvider, Text} from 'react-native-elements';
 import {theme} from './app/config/theme';
 import Router from './app/config/Router';
+import AsyncStorage from '@react-native-community/async-storage';
+import {setIsLogged} from './app/redux/user/actions';
+import api from './app/config/api';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  _retrieveData = async () => {
+    // Se tivermos a token adicionamos no header do axios
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token !== null) {
+        api.defaults.headers.common.Authorization = token;
+        store.dispatch(setIsLogged(true));
+      }
+      this.setState({loading: false});
+    } catch (error) {
+      this.setState({loading: false});
+      // Error retrieving data
+    }
+  };
+  componentDidMount() {
+    this._retrieveData();
+  }
   render() {
+    const {loading} = this.state;
+    if (loading) {
+      return <Text>Teste</Text>;
+    }
     return (
       <Provider store={store}>
         <ThemeProvider theme={theme}>
